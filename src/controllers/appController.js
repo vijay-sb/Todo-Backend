ebf6ee25aa9072df0b6bbe437cd74d5f5807f582
vijaysb0613 <vijaysb2006@gmail.com>
2025-04-24 18:42:19 +0530

    Controllers for all routes

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -7,4 +7,27 @@ export const getTodo = async(res, req) =>
 
 }
 
+export const createTodo = async(res, req) =>
+{
+  const { title } = req.body;
+  const { rows } = await pool.query('INSERT into todos (title) values ($1) RETURNING *',[title]);
+  res.status(201).json(rows[0]);
+}
+
+export const updateTodo = async(res, req)=>
+{
+  const { id } = req.params;
+  const { commpleted } = req.body;
+  const { rows } = await pool.query('UPDATE todos SET completed = $1 WHERE id = $2 RETURNING *',
+    [completed, id]);
+  res.json(rows[0]);
+};
+
+export const deleteTodo = async(res , req) =>
+{
+  const {id} = req.params;
+  await pool.query('DELETE FROM todos WHERE id = ($1)',[id]);
+  res.sendStatus(204);
+};
+
 
